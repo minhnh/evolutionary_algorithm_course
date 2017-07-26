389ca3a16d5650c671034fd2a0917276236f7781
Pranjal Dhole <pranjal.dhole@smail.inf.h-brs.de>
2017-07-26 19:00:59 +0200

    sign error corrected in crowding distance

--- +NSGAII/CrowdingDistance.m
+++ +NSGAII/CrowdingDistance.m
@@ -13,10 +13,10 @@ for nObj = 1:numObjectives
     currentObjectiveName = objectiveNames{nObj};
     sortedFront =  sortByObjective(population(currentFront), currentObjectiveName);
     [sortedFront(1).dist, sortedFront(frontSize).dist] = deal(inf);
-    for j = 2:frontSize-1
+    for j = 2:frontSize - 1
         sortedFront(j).dist = population(j).dist...
-                              + (sortedFront(j-1).(currentObjectiveName)...
-                                 - sortedFront(j+1).(currentObjectiveName)) / maxFitIdeal;
+                              + (sortedFront(j+1).(currentObjectiveName)...
+                                 - sortedFront(j-1).(currentObjectiveName)) / maxFitIdeal;
     end
     for k = 1:frontSize
         genomesWithRanks(currentFront(k)).dist = sortedFront(k).dist;
